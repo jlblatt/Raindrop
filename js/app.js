d65a4a470bd85557c35849ba555dd60bd7bd0a4d
Jason Blatt <jason@jasonblatt.net>
2016-02-05 06:57:08 -0500

    Having fun

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ var FPS = {last: Date.now(), count: 0},
     MOUSE = {last: Date.now(), e: null};
 
 var SONGS = [
-  {path: "midi/mcis.mid", bpm: 86, instruments: ["cello", "cello", "reed_organ", "clarinet"]}
+  {path: "midi/mcis.mid", bpm: 86, instruments: ["acoustic_grand_piano", "cello", "reed_organ", "clarinet"]}
 ];
 
 var SCENE, CAMERA, RENDERER;
@@ -19,11 +19,11 @@ window.onload = function() {
 
   SCENE = new THREE.Scene();
 
-  CAMERA = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-  CAMERA.position.z = 1000;
+  CAMERA = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+  CAMERA.position.z = 650;
 
   RENDERER = new THREE.WebGLRenderer();
-  RENDERER.setSize( window.innerWidth, window.innerHeight );
+  RENDERER.setSize(window.innerWidth, window.innerHeight);
 
   document.body.appendChild( RENDERER.domElement );
 
@@ -58,6 +58,9 @@ window.onload = function() {
           MIDI.Player.resume();
         }
       }
+
+      //skip ahead to test
+      //MIDI.Player.currentTime = 46000;
 
       loop();
 
@@ -115,7 +118,6 @@ function loop() {
 
 function update() {
 
-
 }
 
 ////////////////////////////////
@@ -124,21 +126,95 @@ function update() {
 
 function spawn(note) {
 
-    //no need to do expensive raycasting here since cursor is invisible
+    //need to do this properly at some point
 
-    var size = Math.floor(Math.random() * 640);
-    var r = Math.floor(Math.random() * 256);
-    var b = Math.floor(Math.random() * 256);
-    var g = Math.floor(Math.random() * 256);
+    var x = ((MOUSE.e.clientX / window.innerWidth) - .5) * window.innerWidth;
+    var y = ((MOUSE.e.clientY / window.innerHeight) - .5) * -window.innerHeight;
 
-    var material = new THREE.MeshBasicMaterial({ color: "rgb(" + r + ", " + g + ", " + b + ")", wireframe: true });
-    var circle = new THREE.CircleGeometry(size, 24);
-    var mesh = new THREE.Mesh(circle, material);
-    //mesh.rotation.x += Math.random();
-    //mesh.rotation.y += Math.random();
+    if(note.channel == 0) {
+      var size = (note.velocity + note.note) * Math.random() * 2;
+      var b = Math.floor(192 + (Math.random() * 64));
+      var r = b - 64;
+      var g = b - 64;
+      var opacity = .70 + (Math.random() * .2);
 
-    SCENE.add(mesh);
+      var material = new THREE.MeshBasicMaterial({color: "rgb(" + r + ", " + g + ", " + b + ")", transparent: true, opacity: opacity});
+      var geometry = new THREE.CircleGeometry(size, 72);
+      geometry.translate(x, y, 0);
+      var mesh = new THREE.Mesh(geometry, material);
 
+      SCENE.add(mesh);
+
+      function drop(mesh) {
+        mesh.material.opacity -= .0125;
+        if(mesh.material.opacity < 0) {
+          SCENE.remove(mesh);
+        } else {
+          setTimeout(function(){
+            drop(mesh);
+          }, 16);
+        }
+      }
+
+      drop(mesh);
+    }
+
+    else if(note.channel == 1) {
+      var size = (note.velocity + note.note) * Math.random() * 4;
+      var r = Math.floor(128 + (Math.random() * 128));
+      var g = r - 64;
+      var b = r - 64;
+      var opacity = .40 + (Math.random() * .2);
+
+      var material = new THREE.MeshBasicMaterial({color: "rgb(" + r + ", " + g + ", " + b + ")", transparent: true, opacity: opacity, wireframe: true});
+      var geometry = new THREE.CircleGeometry(size, 32);
+      var mesh = new THREE.Mesh(geometry, material);
+
+      SCENE.add(mesh);
+
+      function drop(mesh) {
+        mesh.material.opacity -= .003;
+        mesh.rotation.z -= .03;
+        if(mesh.material.opacity < 0) {
+          SCENE.remove(mesh);
+        } else {
+          setTimeout(function(){
+            drop(mesh);
+          }, 16);
+        }
+      }
+
+      drop(mesh);
+    }
+
+    else {
+      var size = (note.velocity + note.note) * Math.random() * 4;
+      var g = Math.floor(128 + (Math.random() * 128));
+      var r = g - 64;
+      var b = g - 64;
+      var opacity = .40 + (Math.random() * .2);
+
+      var material = new THREE.MeshBasicMaterial({color: "rgb(" + r + ", " + g + ", " + b + ")", transparent: true, opacity: opacity, wireframe: true});
+      var geometry = new THREE.CircleGeometry(size, 32);
+      geometry.translate(x, y, 0);
+      var mesh = new THREE.Mesh(geometry, material);
+
+      SCENE.add(mesh);
+
+      function drop(mesh) {
+        mesh.material.opacity -= .003;
+        mesh.rotation.z += .03;
+        if(mesh.material.opacity < 0) {
+          SCENE.remove(mesh);
+        } else {
+          setTimeout(function(){
+            drop(mesh);
+          }, 16);
+        }
+      }
+
+      drop(mesh);
+    }
 
 }
 
