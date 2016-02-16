b405c1467e5f98ba609d2556fce12d1d046f5400
Jason Blatt <jason@jasonblatt.net>
2016-02-15 19:31:24 -0500

    Blank effect template

---
+++ js/effects/blank.js
@@ -0,0 +1,23 @@
+EFFECTS['blank'] = {
+
+  spawn: function(note) {
+
+  },
+
+  despawn: function(note) {
+
+  },
+
+  click: function() {
+
+  },
+
+  input: function() {
+
+  },
+
+  tick: function() {
+
+  }
+
+};
