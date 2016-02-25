cd389db91d64c8eb09982c65a3cfe2761c226be4
Jason Blatt <jason@jasonblatt.net>
2016-02-24 20:29:12 -0500

    Song - Greensleeves

--- README.md
+++ README.md
@@ -18,6 +18,7 @@ Move your mouse around.  Hold your mouse down.  Press any/all keys on your keybo
 
 - *Mellon Collie and the Infinite Sadness* - The Smashing Pumpkins (via [midiworld.com](http://www.midiworld.com/files/1105/))
 - *Flight of the Bumblebee* - Nikolai Rimsky-Korsakov (via [8notes.com](http://www.8notes.com/scores/2859.asp?ftype=midi))
+- *Greensleeves* - Unknown Composer (via [tripod.com](http://xaa.tripod.com/PBMusic.htm))
 
 #### Instrument Support
 
--- js/songs.js
+++ js/songs.js
@@ -1,19 +1,28 @@
 var SONGS = [
+  // {
+  //   path: "midi/mellon-collie-and-the-infinite-sadness.mid",
+  //   bpm: 86,
+  //   effectMapping: {
+  //     globals: ['gravity_petals'],
+  //     channels: {
+  //       0: 'basic_raindrop'
+  //     }
+  //   }
+  // },
+  // {
+  //   path: "midi/flight-of-the-bumblebee.mid",
+  //   bpm: 150,
+  //   effectMapping: {
+  //     globals: ['basic_raindrop']
+  //   }
+  // },
   {
-    path: "midi/mellon-collie-and-the-infinite-sadness.mid",
-    bpm: 86,
+    path: "midi/greensleeves.mid",
+    bpm: 70,
+    wadsworth: 15,
     effectMapping: {
-      globals: ['gravity_petals'],
-      channels: {
-        0: 'basic_raindrop'
-      }
-    }
-  },
-  {
-    path: "midi/flight-of-the-bumblebee.mid",
-    bpm: 150,
-    effectMapping: {
-      globals: ['basic_raindrop']
+      globals: ['basic_raindrop'],
+      channels: {}
     }
   }
 ];
